c27c66eb80068e9c57346ef34f3eec3526b0c414
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-14 15:06:16 +0200

    inizio

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+// Creare un array con un insieme di indirizzi email.
+var list_email = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "minnie@gmail.com", "mickey@gmail.com"];
+console.log(list_email);
+
+// Chiedere all'utente il suo indirizzo email e verificare se è nella lista (tramite un ciclo for).
+
+var tuo_email = prompt("Inserire il tuo email");
+console.log(tuo_email);
+for (var i = 0; i < list_email.length; i++) {
+    list_email[i]
+}
+// Comunicare quindi un messaggio appropriato (sei in lista/non sei in lista)
+
+}
